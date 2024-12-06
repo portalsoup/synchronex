1937f277d57c5750e0940bd6d5a7c0c1ebac1b7f
Julian Cleary <portalsoup@gmail.com>
2024-12-05 19:03:39 -0800

    Return to real main method, no subcommands are enabled

--- main.go
+++ main.go
@@ -2,46 +2,12 @@ package main
 
 import (
 	"log"
-	"synchronex/common"
-	"synchronex/schema"
+	"synchronex/command"
 )
 
-//func main() {
-//	err := command.Execute()
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//}
-
 func main() {
-	state := schema.Nex{
-		Files: []schema.File{
-			schema.File{
-				Source:      "synchonex-kt/build.gradle.kts",
-				Destination: "~/.cache/synchronex/build.gradle.kts",
-			},
-			schema.File{
-				Source:      "synchonex-kt/gradle.properties",
-				Destination: "~/.cache/synchronex/gradle.properties",
-				User:        "root",
-			},
-		},
+	err := command.Execute()
+	if err != nil {
+		log.Fatalln(err)
 	}
-	plan := schema.Nex{
-		User: "test",
-		Files: []schema.File{
-			schema.File{
-				Source:      "synchonex-kt/build.gradle.kts",
-				Destination: "~/.cache/synchronex/build.gradle.kts",
-			},
-			schema.File{
-				Source:      "synchonex-kt/gradle.properties",
-				Destination: "~/.cache/synchronex/gradle.properties",
-			},
-		},
-	}
-
-	result := plan.DifferencesFromState(state)
-	log.Printf("Planned changes:\n\n%s", common.PrintPretty(result))
-	common.WriteStatefile(plan)
 }
